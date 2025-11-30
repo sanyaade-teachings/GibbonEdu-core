b7ba73cc6eb93a8af6c4a6ea9d67de8ad7da7c72
Sandra Kuipers <sandra@skuipers.com>
2025-11-30 19:56:09 +0800

    System: upgrade TinyMCE to version 8 and improve the text editor (#2034)

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -53,3 +53,420 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+/**
+ * TinyMCE
+ *
+ * Create pre-configured objects for different Editor modes, to be loaded 
+ * as needed in the Editor.template.php file. Hook into settings using 
+ * Gibbon.config.tinymce from index.php.
+ */
+const gibbonTinyMCEFileUpload = {
+    title: Gibbon.config.tinymce.upload_file,
+    body: {
+        type: "panel",
+        items: [
+            {
+                type: "urlinput",
+                name: "file_upload",
+                filetype: "file",
+                label: Gibbon.config.tinymce.select_file_label,
+                picker_text: Gibbon.config.tinymce.select_file,
+            },
+        ],
+    },
+    buttons: [
+        {
+            type: "cancel",
+            name: "closeButton",
+            text: Gibbon.config.tinymce.cancel,
+        },
+        {
+            type: "submit",
+            name: "submitButton",
+            text: Gibbon.config.tinymce.upload,
+            buttonType: "primary",
+        },
+    ],
+    onSubmit: (api) => {
+        const data = api.getData();
+
+        if (data.file_upload.value == '') {
+            tinymce.activeEditor.notificationManager.open({ text: Gibbon.config.tinymce.invalid_file, type: 'warning' });
+            api.close();
+            return;
+        }
+
+        const notification = tinymce.activeEditor.notificationManager.open({ text: Gibbon.config.tinymce.uploading, progressBar: true });
+        tinymce.activeEditor.setProgressState(true);
+
+        const success = function (location) {
+            const filename = location.split('/').pop();
+            tinymce.activeEditor.setProgressState(false);
+            tinymce.activeEditor.notificationManager.close();
+            tinymce.activeEditor.execCommand("mceInsertContent", false, `<p><a href="${location}" data-fileupload="${data.file_upload.meta.title ?? location}">${data.file_upload.meta.title ?? filename}</a></p>`);
+
+            api.close();
+        };
+
+        const failure = function (message) {
+            tinymce.activeEditor.setProgressState(false);
+            tinymce.activeEditor.notificationManager.close();
+            tinymce.activeEditor.notificationManager.open({ text: message, type: 'error' });
+            api.close();
+        };
+
+        var xhr, formData;
+        xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        // If a value is provided, check that it exists
+        if (!data.file_upload.meta.title) {
+            try {
+                xhr.open('HEAD', data.file_upload.value, false); 
+                xhr.send(null);
+
+                if (xhr.status === 200) {
+                    success(data.file_upload.value);
+                    return;
+                } else {
+                    failure(Gibbon.config.tinymce.error + ": " + xhr.status);
+                    return;
+                }
+            } catch (error) {
+                failure(Gibbon.config.tinymce.error + ": " + error);
+                return;
+            }
+        }
+        
+        xhr.open("POST", "./modules/User/form_editor_uploadAjaxProcess.php");
+
+        xhr.upload.onprogress = function (e) {
+            notification.progressBar.value((e.loaded / e.total) * 100);
+        };
+
+        xhr.onload = function () {
+            var json;
+
+            if (xhr.status === 403) {
+                failure(Gibbon.config.tinymce.error + ": " + xhr.status);
+                return;
+            }
+
+            if (xhr.status < 200 || xhr.status >= 300) {
+                failure(Gibbon.config.tinymce.error + ": " + xhr.status);
+                return;
+            }
+
+            json = JSON.parse(xhr.responseText);
+
+            if (!json || typeof json.location != "string") {
+                failure(Gibbon.config.tinymce.error + ": " + xhr.responseText);
+                return;
+            }
+
+            success(json.location);
+        };
+
+        xhr.onerror = function () {
+            failure(Gibbon.config.tinymce.error + ": " + +xhr.status);
+        };
+
+        try {
+            const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            const blobInfo = blobCache.get(data.file_upload.meta.id);
+
+            formData = new FormData();
+            formData.append("file", blobInfo.blob(), data.file_upload.meta.title );
+
+            xhr.send(formData);
+        } catch (e) {
+            failure(Gibbon.config.tinymce.invalid_file);
+        }
+    },
+};
+
+const gibbonTinyMCEDefaults = {
+    language: Gibbon.config.tinymce.locale,
+    directionality: Gibbon.config.tinymce.locale_rtl,
+    license_key: 'gpl',
+    width: '100%',
+    resize: true,
+    branding: false,
+    onboarding: false,
+    promotion: false,
+    browser_spellcheck: true,
+    convert_urls: false,
+    relative_urls: false,
+    
+    valid_elements: Gibbon.config.tinymce.valid_elements,
+    extended_valid_elements : Gibbon.config.tinymce.extended_valid_elements,
+    invalid_elements: '',
+
+    link_default_target: "_blank",
+    link_context_toolbar: true,
+    link_quicklink: true,
+
+    image_advtab: true,
+    images_upload_url: './modules/User/form_editor_uploadAjaxProcess.php',
+    images_upload_credentials: true,
+    // image_class_list: [
+    //     { title: 'None', value: '' },
+    //     { title: 'No border', value: 'img_no_border' },
+    //     { title: 'Green border', value: 'img_green_border' },
+    //     { title: 'Blue border', value: 'img_blue_border' },
+    //     { title: 'Red border', value: 'img_red_border' }
+    //   ],
+
+    init_instance_callback: (editor) => {
+        // Enable validation checking
+        editor.on('blur', (e) => {
+            tinymce.triggerSave();
+            e.target.targetElm.dispatchEvent(new Event('blur'));
+        });
+    }
+};
+
+const gibbonTinyMCEMinimal = {
+    menubar : false,
+    toolbar: false,
+    statusbar: false,
+    contextmenu: 'cut copy paste pastetext | searchreplace | link | table styles fontfamily fontsize lineheight | forecolor backcolor | removeformat | code preview',
+
+    plugins: 'autoresize table lists link image media quickbars code preview searchreplace',
+    quickbars_selection_toolbar: 'bold italic underline | quicklink | h1 h2 h3 | alignleft aligncenter alignright | bullist numlist |  code',
+    quickbars_insert_toolbar: 'quickimage media quicktable blockquote hr',
+    quickbars_image_toolbar: 'alignleft aligncenter alignright',
+
+    autoresize_bottom_margin: 0,
+};
+
+const gibbonTinyMCEInline = {
+    inline: true,
+    plugins: 'table lists link image media quickbars',
+};
+
+const gibbonTinyMCEFull = {
+    statusbar: true,
+    menubar : 'file edit view insert format table html',
+    contextmenu: 'cut copy paste pastetext | searchreplace | link | table | removeformat | code preview ',
+    plugins: 'autosave table lists link image media quickbars wordcount charmap fullscreen code preview searchreplace',
+    
+    menu: {
+        view: { title: Gibbon.config.tinymce.view, items: 'code wordcount | preview fullscreen' },
+        html: { title: Gibbon.config.tinymce.html, items: 'code preview' },
+    },
+
+    toolbar_mode: 'floating',
+    toolbar_groups: {
+        formatting: {
+          icon: 'typography',
+          items: 'forecolor backcolor | h1 h2 h3 strikethrough blockquote | superscript subscript | removeformat'
+        },
+        styling: {
+            icon: 'paragraph',
+            items: 'blocks fontfamily fontsizeinput lineheight'
+        },
+        alignment: {
+            icon: 'align-left',
+            items: 'alignleft aligncenter alignright alignjustify | indent outdent'
+        },
+        upload: {
+            icon: 'add-file',
+            items: 'image media fileupload'
+        },
+    },
+    toolbar: 'togglemenubar | bold italic underline formatting | styling link | alignment bullist numlist | upload table | charmap hr | code preview fullscreen',
+
+    quickbars_selection_toolbar: false,
+    quickbars_insert_toolbar: false,
+    quickbars_image_toolbar: 'image | alignleft aligncenter alignright | imagedownload imagedelete',
+
+    apply_source_formatting : true,
+    autosave_restore_when_empty: true,
+
+    file_picker_types: 'file image media',
+
+    /* and here's our custom image picker*/
+    file_picker_callback: (cb, value, meta) => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+
+        if (meta.filetype == 'image') {
+            input.setAttribute('accept', 'image/*');
+        }
+
+        input.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+                const id = 'blobid' + (new Date()).getTime();
+                const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                const base64 = reader.result.split(',')[1];
+                const blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                /* call the callback and populate the Title field with the file name */
+                cb(blobInfo.blobUri(), { title: file.name, id: id });
+            });
+            reader.readAsDataURL(file);
+        });
+
+        input.click();
+    },
+    
+    color_map: [
+        "#BFEDD2", "Light Green", 
+        "#FBEEB8", "Light Yellow", 
+        "#F8CAC6", "Light Red", 
+        "#ECCAFA", "Light Purple", 
+        "#C2E0F4", "Light Blue", 
+        "#2DC26B", "Green", 
+        "#F1C40F", "Yellow", 
+        "#FF0000", "Red", 
+        "#B96AD9", "Purple", 
+        "#3598DB", "Blue", 
+        "#169179", "Dark Turquoise", 
+        "#E67E23", "Orange", 
+        "#BA372A", "Dark Red", 
+        "#843FA1", "Dark Purple", 
+        "#236FA1", "Dark Blue", 
+        "#ECF0F1", "Light Gray", 
+        "#CED4D9", "Medium Gray", 
+        "#95A5A6", "Gray", 
+        "#7E8C8D", "Dark Gray", 
+        "#34495E", "Navy Blue", 
+        "#000000", "Black", 
+        "#ffffff", "White", 
+    ],
+
+    setup: function (editor) {
+        editor.ui.registry.addButton("togglemenubar", {
+          tooltip: Gibbon.config.tinymce.advanced_options,
+          icon: "settings",
+          onAction: function () {
+            const menubar = editor.getContainer().querySelector('.tox-menubar');
+            if (menubar) {
+                menubar.style.display = menubar.style.display == 'flex' ? 'none' : 'flex';
+            }
+          },
+        });
+
+        editor.ui.registry.addButton('fileupload', {
+            icon: 'new-document',
+            tooltip: Gibbon.config.tinymce.upload_file,
+            onAction: () => editor.windowManager.open(gibbonTinyMCEFileUpload)
+        });
+
+        editor.ui.registry.addButton('imagedownload', {
+            icon: 'save',
+            tooltip: Gibbon.config.tinymce.download,
+            onAction: function() {
+                const node = editor.selection.getNode();
+                downloadLink = document.createElement('a');
+                downloadLink.href = node.src;
+                downloadLink.setAttribute('download', '');
+                downloadLink.target = 'downloadIframe';
+                downloadLink.click();
+            }
+        });
+
+        editor.ui.registry.addButton('imagedelete', {
+            icon: 'remove',
+            tooltip: Gibbon.config.tinymce.delete,
+            onAction: function() {
+                const node = editor.selection.getNode();
+                editor.dom.remove(node);
+            }
+        });
+
+        const isFileLinkElement = (node) => {
+            return node.nodeName.toLowerCase() === 'a' && node.href && node.dataset.fileupload;
+          }
+      
+          const getFileLinkElement = () => {
+            const node = editor.selection.getNode();
+            return isFileLinkElement(node) ? node : null;
+          };
+      
+          editor.ui.registry.addContextForm("fileedit", {
+              launch: {
+                  type: "contextformbutton",
+                  icon: "new-document",
+              },
+              label: Gibbon.config.tinymce.file,
+              predicate: isFileLinkElement,
+              initValue: () => {
+                  const elm = getFileLinkElement();
+                  return !!elm ? elm.dataset.fileupload : "";
+              },
+              commands: [
+                  {
+                      type: "contextformbutton",
+                      icon: "new-tab",
+                      tooltip: Gibbon.config.tinymce.open,
+                      primary: true,
+                      onAction: (formApi) => {
+                          const elm = getFileLinkElement();
+                          window.open(elm.href, "_blank");
+                          formApi.hide();
+                      },
+                  },
+                  {
+                      type: "contextformbutton",
+                      icon: "save",
+                      tooltip: Gibbon.config.tinymce.download,
+                      onAction: (formApi) => {
+                            const elm = getFileLinkElement();
+
+                            downloadLink = document.createElement('a');
+                            downloadLink.href = elm.href;
+                            downloadLink.setAttribute('download', '');
+                            downloadLink.target = 'downloadIframe';
+                            downloadLink.click();
+
+                            formApi.hide();
+                      },
+                  },
+                  {
+                      type: "contextformbutton",
+                      icon: "remove",
+                      tooltip: Gibbon.config.tinymce.delete,
+                      onAction: (formApi) => {
+                        editor.windowManager.confirm(Gibbon.config.tinymce.delete_confirm, (state) => {
+                            if (!state) return;
+                            const elm = getFileLinkElement();
+                            editor.dom.remove(elm);
+                          });
+                          
+                          formApi.hide();
+                      },
+                  },
+              ],
+          });
+      },
+
+    init_instance_callback: (editor) => {
+        // Enable quick save from within tinymce
+        editor.addShortcut("meta+s", "Custom Ctrl+S", function (e) {
+            editor.formElement.dispatchEvent(new Event('quicksave'));
+        });
+
+        // Enable validation checking
+        editor.on('blur', (e) => {
+            tinymce.triggerSave();
+            e.target.targetElm.dispatchEvent(new Event('blur'));
+        });
+
+        // Autosave trigger
+        if (editor.targetElm.hasAttribute('data-autosave')) {
+            setTimeout(function () {
+                editor.on('keydown', function () {
+                    tinymce.triggerSave();
+                    gibbonFormSubmitQuiet(document.getElementById(editor.formElement.id), editor.targetElm.getAttribute('data-autosave'))
+                })
+            }, 100);
+        }
+    }
+};
